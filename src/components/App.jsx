38d30cc8ab413b85a822afb9ac5d72e45c07ba09
Valerii <vd.alternative@gmail.com>
2024-01-23 22:14:56 +0200

    set

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
 };
+
+
+
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzU3MTRjMGE4MGU2YTg5NDNkYTc2OGMwYzIwODVjOSIsInN1YiI6IjY1YWU5NGIwYWFkOWMyMDBjYTVhMDg5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.66FtwdH_dSdO7_6e-VsDPrhsmKfe-1TKJg0wb5pXm18
+
+//535714c0a80e6a8943da768c0c2085c9
