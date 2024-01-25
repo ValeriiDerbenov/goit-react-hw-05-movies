c08d51d3b982d476d1f9680a50e32283bbb28a45
Valerii <vd.alternative@gmail.com>
2024-01-25 16:09:44 +0200

    change

---
+++ src/components/api/Api.js
@@ -0,0 +1,17 @@
+//535714c0a80e6a8943da768c0c2085c9
+
+import axios from 'axios';
+
+const API_KEY = '535714c0a80e6a8943da768c0c2085c9';
+
+export const fetchMovies = async () => {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/trending/movie/day',
+    {
+      params: {
+        api_key: API_KEY,
+      },
+    }
+  );
+  return response.data.results;
+};
