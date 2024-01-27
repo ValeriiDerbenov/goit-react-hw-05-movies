a9c925467e8f31506908d965c391ce91c039c67b
Valerii <vd.alternative@gmail.com>
2024-01-27 21:18:27 +0200

    Update ReviewsPage.jsx

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react"
+import { fetchReviews } from "components/api/Api";
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 
 const Reviews = () => {
 	const [reviews, setReviews] = useState([]);
 	const {id} = useParams();
+	const [loading, setLoading] = useState([]);
+	const [error, setError] = useState([]);
 	
-	
+	useEffect(() => {
+    const getMovieReviews = async () => {
+      try {
+				setLoading(true);
+        setReviews(await fetchReviews(id));
+      } catch (error) {
+        setError(error.message);
+      } finally {
+				setLoading(false);
+			}
+    };
+    getMovieReviews();
+  }, [id]);
 
 	return (
+	<>
+		{loading && <p>...Loading</p>}
+		{error && <p>{error}</p>}
 		<h2>Reviews</h2>
+		<ul>
+			{reviews.length === 0 ? (
+				<p>We don't have any reviews for this movie.</p>
+			) : (
+				reviews.map(review => (
+					<li key={review.id}>
+						<h3>Author: {review.author}</h3>
+						<p>{review.content}</p>
+					</li>
+				))
+			)}
+		</ul>
+	</>
 	)
 }
 
