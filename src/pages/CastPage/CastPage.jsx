41a218f62c69866116951c84e357d1c9fbd815e9
Valerii <vd.alternative@gmail.com>
2024-01-27 19:45:00 +0200

    Update CastPage.jsx

--- src/pages/CastPage/CastPage.jsx
+++ src/pages/CastPage/CastPage.jsx
@@ -1,7 +1,45 @@
+import { fetchMovieCast } from "components/api/Api";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Cast = () => {
+
+	const [cast, setCast] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getMovieCast = async () => {
+      try {
+        setCast(await fetchMovieCast(id));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovieCast();
+  }, [id]);
+
 	return (
+		
+		<>
 		<h2>Cast</h2>
+		<ul>
+			{cast.map(actor => (
+				<li key={actor.id}>
+					<img
+						src={
+							actor.profile_path
+								? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+								: `https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png`
+						}
+						alt={actor.name}
+						width="150"
+					/>
+					<p>{actor.name}</p>
+					<p>Character: {actor.character}</p>
+				</li>
+			))}
+		</ul>
+	</>
 	)
 }
 
